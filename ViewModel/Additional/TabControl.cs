e49566e3e20704d598e8be5ecd96934e25217513
AlexanderZhukov1 <ranedir@live.com>
2017-11-13 00:25:29 +0300

    "THE DARK SHOULD FEAR ME!"

--- ViewModel/Additional/TabControl.cs
+++ ViewModel/Additional/TabControl.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Windows.Media.Imaging;
-using MaterialDesignThemes.Wpf;
 using Model;
 using Model.OperatorsHelper;
 

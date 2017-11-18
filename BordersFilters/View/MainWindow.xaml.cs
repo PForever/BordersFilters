c09ec35f77033d12a67da324696268cec8668dc4
AlexanderZhukov1 <ranedir@live.com>
2017-11-18 04:03:04 +0300

    Add some borders

--- BordersFilters/View/MainWindow.xaml.cs
+++ BordersFilters/View/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace BordersFilters.View
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow //: Window
     {
         private readonly ViewModel.ViewModel _viewModel;
 
